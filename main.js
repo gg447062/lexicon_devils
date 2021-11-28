1d48c06b02e2cfdd7b7260a02b75d21a7339d799
gg447062 <gg447062@gmail.com>
2021-11-27 21:09:17 -0600

    updates assets and design

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
-const wrappers = document.querySelectorAll('div.link-wrapper');
 const links = document.querySelectorAll('a');
 const devil = document.getElementById('big-devil');
-const container = document.getElementById('links-container');
-const linksBody = document.getElementById('links-body');
+const chat = document.getElementById('chat_container');
+const chatbox = document.getElementById('devil-chat');
+const fg = document.getElementById('fg_img');
+const bg = document.getElementById('bg_img');
 const devilSpeech = document.getElementById('devil-speech');
 const flyingDevil = document.getElementById('flying-devil');
 const body = document.querySelector('body');
-const c1 = document.getElementById('c1');
+const headerLink = document.getElementById('header-link');
+
 let spoken = false;
+let display = true;
+const heights = { true: ['600px', '300px'], false: ['300px', '0px'] };
+const opacityStyle = { true: '1', false: '0' };
 let time = 0;
-let x = 200;
-let flipped = false;
 
 const devilWords = {
   0: `the chat is over there dummy i'm just a bot`,
@@ -22,75 +25,56 @@ const devilWords = {
       i think you should try the twitter.`,
 };
 
-wrappers.forEach((wrapper) => {
-  wrapper.addEventListener('mouseenter', () => {
-    const link = wrapper.children[0];
+headerLink.onclick = () => {
+  flyingDevil.setAttribute('src', 'assets/devil_hands.png');
+};
 
-    link.style.transform = 'scaleX(-1.5)';
-  });
-});
+// wrappers.forEach((wrapper) => {
+//   wrapper.addEventListener('mouseenter', () => {
+//     const link = wrapper.children[0];
 
-wrappers.forEach((wrapper) => {
-  wrapper.addEventListener('mouseleave', () => {
-    const link = wrapper.children[0];
-    link.style.transform = 'scaleX(1)';
-  });
-});
+//     link.style.transform = 'scaleX(-1.5)';
+//   });
+// });
+
+// wrappers.forEach((wrapper) => {
+//   wrapper.addEventListener('mouseleave', () => {
+//     const link = wrapper.children[0];
+//     link.style.transform = 'scaleX(1)';
+//   });
+// });
 
 document.addEventListener('mousemove', (e) => {
   const midX = window.innerWidth / 2;
   const midY = window.innerHeight / 2;
-  const mouseX = e.pageX;
-  const mouseY = e.pageY;
-  const diffX = midX - mouseX;
-  const diffY = midY - mouseY;
-  const midHeight = parseInt(container.style.height) / 2;
-  const midWidth = parseInt(container.style.width) / 2;
-  const offsetX = midX - midWidth;
+  const diffX = midX - e.pageX;
+  const diffY = midY - e.pageY;
+  const midHeight = fg.height / 2;
+  const fgMidWidth = fg.width / 2;
+  const bgMidWidth = bg.width / 2;
+  const bgOffsetX = midX - bgMidWidth;
+  const fgOffsetX = midX - fgMidWidth;
   const offsetY = midY - midHeight;
 
-  container.style.top = `${offsetY + diffY / 15}px`;
-  container.style.left = `${offsetX + diffX / 20}px`;
+  fg.style.top = `${offsetY + diffY / 15}px`;
+  fg.style.left = `${fgOffsetX + diffX / 20}px`;
+  bg.style.top = `${offsetY - diffY / 15}px`;
+  bg.style.left = `${bgOffsetX - diffX / 20}px`;
   body.style.backgroundPosition = `${diffX / 100}% ${diffY / 200}%`;
-
-  container.style.boxShadow = `
-  ${diffX / 20}px ${diffY / 30}px rgb(30, 30, 220)`;
 });
 
-devil.addEventListener('mouseenter', () => {
-  if (!spoken) {
-    const idx = Math.floor(Math.random() * 5);
-    const statement = devilWords[idx];
-    devilSpeech.innerHTML = statement;
-    devilSpeech.style.opacity = '1';
-    spoken = true;
-    setTimeout(() => {
-      devilSpeech.style.opacity = '0';
-    }, 3000);
-    setTimeout(() => {
-      devilSpeech.innerHTML = '';
-      spoken = false;
-    }, 4000);
-  }
-});
+devil.onclick = () => {
+  display = !display;
+  chat.style.height = heights[display][0];
+  chatbox.style.opacity = opacityStyle[display];
+  chatbox.style.height = heights[display][1];
+};
 
 function bounce() {
   let y = 5 * Math.sin(time / 20);
-  devil.style.bottom = `${y + 20}px`;
+  devil.style.top = `${y - 20}px`;
   time++;
   requestAnimationFrame(bounce);
 }
 
-function fly() {
-  const increment = { true: -2, false: 2 };
-  flyingDevil.style.top = `${x}px`;
-  if (x === window.innerHeight - 102 || x === 128) {
-    flipped = !flipped;
-  }
-  x += increment[flipped];
-
-  requestAnimationFrame(fly);
-}
-
-fly();
 bounce();
