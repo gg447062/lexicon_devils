a47ee72d37b264656b4c9e2377ae4b42615c3a80
gg447062 <gg447062@gmail.com>
2021-12-27 15:08:40 -0700

    updates express server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use('/', express.static(__dirname + '/'));
+app.use('/', express.static(__dirname + '/public'));
 
 const PORT = process.env.PORT || 8080;
 
