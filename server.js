727667804e60ba39c09fe1f92ca160873a371ff8
Gabriel Gutierrez <gg447062@gmail.com>
2022-11-08 10:06:47 -0600

    adds https redirect middleware

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      res.redirect(301, `https://${req.headers.host}${req.originalUrl}`);
+    } else {
+      next();
+    }
+  });
+}
+
 app.use('/', express.static(__dirname + '/public'));
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
